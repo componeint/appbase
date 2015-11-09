58b2b872a340743d06ec8834c7d885e843641bd9
anonymoussc <50c5ac69@opayq.com>
2015-11-09 18:53:44 +0700

    Iced :shaved_ice:

--- public/src/scripts/appbase/src/components/core-components/coreComponents.example.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by consigliere on August 18th 2015, 4:30:13 pm.
- */
-
-angular.module('appbase.coreComponents', ['']);
--- public/src/scripts/appbase/src/provides/constants/appbase.example.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by consigliere on August 18th 2015, 4:30:13 pm.
- */
-
-//appbase
-//    .constant('{name}', '{value}');
