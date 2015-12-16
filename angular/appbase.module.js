e2065730693e6cda03a06dbd8eb5cbcb87066b4c
anonymoussc <50c5ac69@opayq.com>
2015-12-16 16:00:06 +0700

    Add directory

---
+++ angular/appbase.module.js
@@ -0,0 +1,15 @@
+/**
+ * Created by anonymous on 08/12/15 16:58.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('appbase', [
+            /* Shared */
+            'appFoundation',
+            'widgets'
+        ]);
+
+})();
