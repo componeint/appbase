b2b378adf775d14fbfbf9fb450546cc1c73c0ede
anonymoussc <50c5ac69@opayq.com>
2016-03-10 08:15:59 +0700

    Modified AppbaseServiceProvider

--- src/Appbase/AppbaseServiceProvider.php
+++ src/Appbase/AppbaseServiceProvider.php
@@ -43,8 +43,9 @@ class AppbaseServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->register(\Componeint\Dashboard\DashboardServiceProvider::class);
+        $this->app->register(\Componeint\AppFoundation\AppFoundationServiceProvider::class);
         $this->app->register(\Componeint\Seneschal\SeneschalServiceProvider::class);
+        $this->app->register(\Componeint\Dashboard\DashboardServiceProvider::class);
     }
 
     /**
